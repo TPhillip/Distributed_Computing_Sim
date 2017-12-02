7c6e81f00c90cbb80264d6e2d1a961dc743087d1
messenio <alias.tdp@gmail.com>
2017-12-02 18:10:20 -0500

    Added Metrics class (for real this time)

---
+++ src/com/distributed/phase2/Metrics.java
@@ -0,0 +1,52 @@
+package com.distributed.phase2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.InetAddress;
+
+public class Metrics {
+    private static Peer[] allPeers;
+    private static String otherPeersName;
+
+    public static void main(String[] args) {
+        int peers = 100;
+        String serverRouterAddress = "messenio";
+        otherPeersName = args[1];
+
+        allPeers = new Peer[peers];
+
+        for (int i = 0; i < peers; i++) {
+            allPeers[i] = new Peer(String.format("peer-%s%d", args[0], i + 1), serverRouterAddress);
+        }
+        System.out.println(allPeers.length);
+        for (int i = 0; i < peers; i++) {
+            SendMsg sendThread = new SendMsg(i, otherPeersName, "README.md");
+            sendThread.start();
+        }
+    }
+
+    private static class SendMsg extends Thread {
+        private String revievingPeer;
+        private String fileName;
+        private int i;
+
+        public SendMsg(int i, String recievingPeer, String fileName) {
+            this.i = i;
+            this.revievingPeer = String.format("peer-%s%d", recievingPeer, i);
+            this.fileName = fileName;
+        }
+
+        public void run() {
+            try {
+                allPeers[i].sendFile(revievingPeer, new File(fileName));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                return;
+            } catch (IOException e) {
+                e.printStackTrace();
+                return;
+            }
+        }
+    }
+}
